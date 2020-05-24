83649e3cbc602254d56a54501b17402a62fa11e2
Laksh Jadhwani <47735648+lakshjadhwanilj@users.noreply.github.com>
2020-05-24 15:12:00 +0530

    Updated google oauth url

--- app.js
+++ app.js
@@ -94,7 +94,8 @@ passport.deserializeUser(function (id, done) {
 passport.use(new GoogleStrategy({
         clientID: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
-        callbackURL: 'http://localhost:3000/auth/google/secrets',
+        // callbackURL: 'http://localhost:3000/auth/google/secrets',
+        callbackURL: 'https://lit-inlet-76032.herokuapp.com/auth/google/secrets',
         userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
     },
     function (accessToken, refreshToken, profile, cb) {
